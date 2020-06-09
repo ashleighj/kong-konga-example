7232b5686a9721f908fc42e23206f910c522192a
Ashleigh Waldek <ashleighwaldek@gmail.com>
2020-06-09 17:40:04 -0400

    customisation

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Setup Kong + Konga using Docker compose
-This repository contains required file for running Kong + Konga using Docker
-you can checkout the article in [here](https://dev.to/vousmeevoyez/setup-kong-konga-part-2-dan)
+
+This is a fork of the repo [here](https://github.com/vousmeevoyez/kong-konga-example) with a customised start script that will save anyone reading the article [here](https://dev.to/vousmeevoyez/setup-kong-konga-part-2-dan) lots of time trying to figure out what the hell is going on, or anyone attempting to run the start script in the original repo a ton of time trying to figure out why everything is broken, i.e. past me.
+
+## Instructions
+
+1. Run the `start.sh` script  
+
+2. Copy the `id` value in the json retrieved from the last `curl` command (the json response will be printed to your terminal)
+
+3. Run this (including the id you just copied in place of the placeholder):
+`curl --location --request POST 'http://localhost:8001/consumers/{the_id_you_just_copied}/key-auth'`
+
+4. You will have received an api key in the last call. Copy it. You'll need it for step 7.
+
+5. Open your browser, navigate to `localhost:9000` to open konga. 
+
+6. Set up an admin user and log in. 
+
+7. Set up a KEY AUTH connection to kong like you see in `setup.png` (details below). Use the api key you got in step 3.
+
+```
+Name: admin-api
+Loopback API URL: http://kong:8000/admin-api
+API KEY: ${the_api_key_you_copied}
+```
+
+**If you experience some trouble, try increasing the sleep time in line 41 of `start.sh`.
--- start.sh
+++ start.sh
@@ -1,4 +1,63 @@
+# KONG SETTINGS
+export KONG_DB_NAME=db_kong
+export KONG_DB_USERNAME=konguser
+export KONG_DB_PASSWORD=kongpassword
+export KONG_DB_HOST=db
+export KONG_DB_PORT=5432
+
+export KONG_DATABASE=postgres
+export KONG_PROXY_ACCESS_LOG=/dev/stdout
+export KONG_ADMIN_ACCESS_LOG=/dev/stdout
+export KONG_PROXY_ERROR_LOG=/dev/stderr
+export KONG_ADMIN_ERROR_LOG=/dev/stderr
+export KONG_ADMIN_LISTEN=0.0.0.0:8001,\ 0.0.0.0:8444\ ssl
+
+export KONG_PROXY_PORT=8000
+export KONG_PROXY_SSL_PORT=8443
+export KONG_PROXY_ADMIN_API_PORT=8001
+export KONG_PROXY_ADMIN_SSL_API_PORT=8444
+
+# KONGA SETTINGS
+export KONGA_DB_NAME=db_konga
+export KONGA_DB_USERNAME=kongauser
+export KONGA_DB_PASSWORD=kongapassword
+export KONGA_DB_HOST=db
+export KONGA_DB_PORT=5432
+
+export KONGA_TOKEN_SECRET=some-secret-token
+export KONGA_ENV=development
+export KONGA_PORT=9000
+
+printf "\n\n ----- Create network ----- \n\n"
+
 docker network create kong-net
-docker-compose up --build -d db kong-migrations
-sleep 10
-docker-compose up --build -d kong konga
+
+printf "\n\n ----- Build/run db, migrations, kong ----- \n\n"
+
+docker-compose up -d --build db kong-migrations kong
+
+printf "\n\n Waiting for kong.... \n\n"
+
+sleep 5
+
+echo " ....Done waiting"
+
+printf "\n\n ----- TEST kong admin api ----- \n\n"
+
+curl -i http://localhost:8001/
+
+printf "\n\n ----- Build/run konga ----- \n\n"
+
+docker-compose up --build -d konga
+
+printf "\n\n ---> Add Admin API route... \n\n"
+
+curl --location --request POST 'http://localhost:8001/services/admin-api/routes' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "paths": ["/admin-api"]
+}'
+
+printf "\n\n ---> Show consumers so you can get the consumer id... ----- \n\n"
+
+curl --location --request GET 'http://localhost:8001/consumers/'
